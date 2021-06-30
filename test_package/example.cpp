a15aa42149d9407527343923661b6a4cdacc6018
arronpj <princejar@gmail.com>
2021-06-30 09:49:42 +0800

    update test function accordingly

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,9 +1,23 @@
 #include "bf_helper.h"
+#include "bf_object.h"
+
+#include <stdio.h> //for printf
+
+void check_library_version()
+{
+    bf_helper_version();
+    const char *ov = bf_object_version();
+    printf("check version, %s\n", ov);
+}
+
+void run_decoder_process()
+{
+    bf_decoder_init();
+    bf_decoder_shutdown();
+}
 
 int main()
 {
-    bf_helper_version();
-
-    bf_decoder_init();
-    bf_decoder_shutdown();
+    check_library_version();
+    run_decoder_process();
 }
