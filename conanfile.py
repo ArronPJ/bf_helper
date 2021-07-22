959a739535c37821c6b2fe9cd6b90b9e31186025
arronpj <princejar@gmail.com>
2021-07-22 09:22:02 +0800

    add CMakeLists.txt to root and change conanfile accordingly.

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools, __version__ as conan_version
+from conans.tools import Version
 
 
 class BfHelperConan(ConanFile):
@@ -9,8 +10,6 @@ class BfHelperConan(ConanFile):
     url = "https://github.com/ArronPJ/bf_helper"
     description = "Library package by Conan"
     topics = ("helper utility", "crossplatform")
-    #requires = "boost/1.71.0" # comma-separated list of requirements
-    #requires = "jansson/2.13.1"
     settings = "os", "compiler", "build_type", "arch"
     options = {
         "shared": [True, False], 
@@ -18,25 +17,53 @@ class BfHelperConan(ConanFile):
     }
     default_options = {
         "shared": False, 
-        "fPIC": True
+        "fPIC": True,
+        "boost:without_wave": True, 
+        "boost:without_test": True,
+        "boost:without_stacktrace": True,
+        "boost:without_iostreams": True, #need random , regex as well
+        #"boost:without_serialization": True, -> date_time need this
+        "boost:without_regex": True,
+        "boost:without_random": True,
+        "boost:without_math": True,
+        "boost:without_log": True,
+        "boost:without_locale": True,
+        "boost:without_graph": True,
+        "boost:without_coroutine": True,
+        "boost:without_contract": True,
+        "boost:without_context": True,  #--- new here(13)
+        #"boost:without_container": True,# thread need this
     }
     # add "conanbuildinfo.json", 
     generators = "cmake", "json"
-    exports_sources = "src/*"
+    #exports_sources = "src/*"
+    exports_sources = "CMakeLists.txt", "src/*"
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
     def requirements(self):
         # If we need to dynamic require according to OS?
-        self.requires("jansson/2.12")
+        self.requires("jansson/2.12")   # new = "jansson/2.13.1"
         self.requires("boost/1.69.0")
 
 
     def build(self):
         cmake = CMake(self)
         cmake.verbose = True
-        cmake.configure(source_folder="src")
+        
+
+        # for android
+        #cmake.definitions["ANDROID_NDK"] = "/aaa/bbb/"
+        
+        #if conan_version >= Version("1.36.0"):
+        #    ...
+        #else:
+        cmake.configure()
+        #cmake.configure(source_folder="src")
+        
+        ## for Xcode IDE Generate fail , unable to replace "Unix Makefiles"
+        #cmake.configure(args=["-G", "Xcode"])
         cmake.build()
 
         # Explicit way:
